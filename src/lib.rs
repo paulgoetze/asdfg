9e9d000dd0a74c31f3de4c9d430f1dd79d065426
Paul Götze <paul.christoph.goetze@gmail.com>
2020-01-07 00:15:33 +0100

    Fix order of lib fn declaration
    Just for readability :)

--- src/lib.rs
+++ src/lib.rs
@@ -5,15 +5,6 @@ mod config;
 use std::error::Error;
 use structopt::StructOpt;
 
-pub fn run() -> Result<(), Box<dyn Error>> {
-    match Cli::from_args().command {
-        Command::Install(command) => command.run()?,
-        Command::Config(command) => command.run()?,
-    }
-
-    Ok(())
-}
-
 #[derive(Debug, StructOpt)]
 #[structopt(name = "asdfg", about = "Installing asdf packages from a YAML config.")]
 pub struct Cli {
@@ -25,4 +16,13 @@ pub struct Cli {
 pub enum Command {
     Install(commands::Install),
     Config(commands::Config),
+}
+
+pub fn run() -> Result<(), Box<dyn Error>> {
+    match Cli::from_args().command {
+        Command::Install(command) => command.run()?,
+        Command::Config(command) => command.run()?,
+    }
+
+    Ok(())
 }
